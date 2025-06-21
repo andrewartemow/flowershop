6ed9fafe951ca77ae209162b81f90366a96ea52c
andrei <andrei@clinicsoftware.com>
2025-06-21 16:55:46 +0100

    commit

--- public/js/fetchProducts.js
+++ public/js/fetchProducts.js
@@ -1,5 +1,5 @@
 export const fetchProducts = async (limit) => {
-  const response = await fetch(`api/products?limit=${limit}`);
+  const response = await fetch(`/api/products?limit=${limit}`);
   const products = await response.json();
 
   return products;
